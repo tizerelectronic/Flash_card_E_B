8228d320e56b8c07b1da6e6324a6a88682712039
Emmanuel Hyppolite <emma0542@gmail.com>
2023-03-01 17:07:42 -0500

    add git image

--- app/src/main/java/com/example/emflashcard/MainActivity.java
+++ app/src/main/java/com/example/emflashcard/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.emflashcard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    TextView r1;
-    TextView r2;
-    TextView r3;
+    Button r1;
+    Button r2;
+    Button r3;
     TextView q1;
     Button bt1;
     LinearLayout l1;
@@ -23,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         r1 = findViewById(R.id.repons_1);
         r2 = findViewById(R.id.repons_2);
         r3 = findViewById(R.id.repons_3);
-        q1 = findViewById(R.id.question1);
+        q1 = findViewById(R.id.question_1);
         bt1 = findViewById(R.id.but1);
         l1 = findViewById(R.id.l1);
 
@@ -35,6 +37,29 @@ public class MainActivity extends AppCompatActivity {
                 l1.setVisibility(View.VISIBLE);
             }
         });
+
+
+
+    }
+
+    public void repons_true(View view) {
+        Intent intent = new Intent(this, reponse.class);
+        startActivity(intent);
+//        l1.setVisibility(View.GONE);
+//        bt1.setVisibility(View.GONE);
+//        q1.setText(R.string.repons1);
+    }
+
+    public void Repons_f(View view) {
+        r2.setVisibility(View.INVISIBLE);
+        bt1.setVisibility(View.INVISIBLE);
+
+
+    }
+
+    public void repons_f2(View view) {
+        r3.setVisibility(View.INVISIBLE);
+        bt1.setVisibility(View.INVISIBLE);
     }
 }
 
